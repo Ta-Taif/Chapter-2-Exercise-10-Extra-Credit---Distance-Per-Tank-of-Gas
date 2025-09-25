919e591e6afcbda6e3a78bb06a9c1b4a9e1a7f6a
Ta-Taif <qtaif10@gmail.com>
2025-09-25 16:07:22 -0400

    Add project files.

---
+++ Chapter 2 Exercise 10 (Extra Credit)- Distance Per Tank of Gas/Chapter 2 Exercise 10 (Extra Credit)- Distance Per Tank of Gas.cpp
@@ -0,0 +1,20 @@
+/*Program: Ocean Levels asiimnet
+    Programmer: Taif
+    Date: 09/25/2025
+    Description: Assingmet caculator
+    */
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	double MPG;
+    const double car = 15.0;
+	double driven = 375.0;
+
+
+    MPG = driven / car;
+    cout << "The car gets " << MPG << " miles per gallon.\n";
+}
+
+
