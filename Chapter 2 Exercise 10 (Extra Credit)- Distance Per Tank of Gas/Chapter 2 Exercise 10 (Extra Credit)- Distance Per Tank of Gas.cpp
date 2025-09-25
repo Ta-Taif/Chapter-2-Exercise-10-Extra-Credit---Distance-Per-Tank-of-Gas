4e7f69570acdf41e3192645801d3f70a1da22145
Ta-Taif <qtaif10@gmail.com>
2025-09-25 16:10:49 -0400

    userinput

--- Chapter 2 Exercise 10 (Extra Credit)- Distance Per Tank of Gas/Chapter 2 Exercise 10 (Extra Credit)- Distance Per Tank of Gas.cpp
+++ Chapter 2 Exercise 10 (Extra Credit)- Distance Per Tank of Gas/Chapter 2 Exercise 10 (Extra Credit)- Distance Per Tank of Gas.cpp
@@ -9,9 +9,12 @@ using namespace std;
 int main()
 {
 	double MPG;
-    const double car = 15.0;
-	double driven = 375.0;
-
+    double car;
+    double driven;
+	cout << "What is number of gallons of gas the car can hold: ";
+	cin >> car; 
+	cout << "How many miles can the car be driven on a full tank: ";
+	cin >> driven;
 
     MPG = driven / car;
     cout << "The car gets " << MPG << " miles per gallon.\n";
